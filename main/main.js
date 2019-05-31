fd3e81d8ddf65125909ecd2df7f0351507fb1c05
linghaunti <upc_edu@yeah.net>
2019-05-31 17:34:08 +0800

    finish the tdd main function

--- main/main.js
+++ main/main.js
@@ -1,4 +1,17 @@
 module.exports = function main(inputs) {
-    // write your code here...
-    return "price";
+    var fee1 = dfee(inputs['distance']);
+    var fee2 = wfee(inputs['parkTime']);
+
+    return Math.round(fee1+fee2);
+};
+
+function dfee (distance) {
+	var fee = 6;
+	fee += distance > 2 ? (distance-2)*0.8 : 0;
+	fee += distance > 8 ? (distance-8)*0.4 : 0;
+	return fee;
+};
+
+function wfee (time) {
+	return 0.25*time;
 };
